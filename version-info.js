e27ce3326fb9d4d1925cdd3d81fcecba85f25090
Košťálek Jan <jkostalek@trask.cz>
2026-04-18 23:59:17 +0200

    Change user button in gfx2 settings

--- version-info.js
+++ version-info.js
@@ -1,8 +1,8 @@
 window.HrrraVersionInfo = Object.freeze({
-  versionCode: 44,
-  versionName: "1.0.44",
+  versionCode: 45,
+  versionName: "1.0.45",
   whatsNew: [
-  "Release bump to version 1.0.44.",
+  "Release bump to version 1.0.45.",
   "Web assets and Android packaging are being resynced from the current root project state.",
   "Current manual project edits are now being prepared for main, web, and Android."
 ]
