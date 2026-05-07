09b2aec9b64049f153c445878c5bb4f0f87a3e7c
Košťálek Jan <jkostalek@trask.cz>
2026-05-07 23:54:48 +0200

    Release 1.0.80

--- www/version-info.js
+++ www/version-info.js
@@ -1,8 +1,8 @@
 window.HrrraVersionInfo = Object.freeze({
-  versionCode: 79,
-  versionName: "1.0.79",
+  versionCode: 80,
+  versionName: "1.0.80",
   whatsNew: [
-  "Release bump to version 1.0.79.",
+  "Release bump to version 1.0.80.",
   "Android rewarded continue is now forced to use Google test ads for internal testing."
 ]
 });
